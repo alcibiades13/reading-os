cadd12c04ffc758d5b5ba6e8b932c346ed41082d
Alcibiades <branislav.teofilovic@gmail.com>
2026-02-02 09:58:22 +0100

    fixing build process

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,8 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Collect static files
+# Collect static files (does not need database)
 python manage.py collectstatic --no-input
 
-# Run migrations
-python manage.py migrate
+# NOTE: migrations moved to preDeployCommand in render.yaml
+# because the internal DB hostname is not available during build
